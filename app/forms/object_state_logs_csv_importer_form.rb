083fc5a02220c60143ce2a902e7865b1adc6f064
Ejay <ejaypcanaria+ejayc@gmail.com>
2019-05-29 00:08:22 +0800

    [FEATURE] Add initial implementation of CSV parsing and persisting the record

--- app/forms/object_state_logs_csv_importer_form.rb
+++ app/forms/object_state_logs_csv_importer_form.rb
@@ -1,6 +1,22 @@
 class ObjectStateLogsCsvImporterForm < BaseForm
+  require 'csv'
+
   attribute :csv_file, File
 
   def save
+    ObjectStateLog.transaction do
+      CSV.parse(csv_file.read.gsub('\"','""')) do |row|
+        object_id, object_type, timestamp, object_changes = row
+
+        ObjectStateLog.create!(
+          object_id: object_id,
+          object_type: object_type,
+          timestamp: Time.at(timestamp.to_i),
+          object_changes: object_changes
+        )
+      end
+    end
+  rescue ActiveRecord::RecordInvalid
+  rescue CSV::MalformedCSVError
   end
 end
